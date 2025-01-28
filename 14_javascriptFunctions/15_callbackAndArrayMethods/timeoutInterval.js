9e5b77b1b34eff895be51fd27aa419a15a83f3ed
mvanvyve <mvanvyve@illumina.com>
2025-01-28 20:01:27 +0100

    add content

--- 14_javascriptFunctions/15_callbackAndArrayMethods/timeoutInterval.js
+++ 14_javascriptFunctions/15_callbackAndArrayMethods/timeoutInterval.js
@@ -1,1 +1,8 @@
 // The setTimeout and setInterval methods are not array methods but they so take a callback function to operate. Which is why it is usefull to cover these here.
+
+const id = setInterval(() => {
+  console.log(Math.random())
+}, 3000);
+
+
+console.log(id);
